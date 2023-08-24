5cef5322923a07ebee7ab59cfd8f353575cd1f32
NickGofman <83462964+NickGofman@users.noreply.github.com>
2023-08-24 21:36:27 +0300

    add my cv

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import cvFile from '../../assets/NickGofman_Resume_.pdf'; // Import the path to your CV file
 
 function CTA() {
   return (
     <div className="flex items-center mt-8 cta">
       <a
-        href="cv"
-        download
+        href={cvFile}
+        download="NickGofman_Resume.pdf"
         className="btn text-color-primary hover:bg-color-white hover:text-color-bg  mr-4 transition-colors duration-300 ease-in-out p-4 rounded-md border-2"
       >
         Download CV
