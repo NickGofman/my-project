c16e8a9c496cb8959c9ea45fedaea5e399085978
NickGofman <83462964+NickGofman@users.noreply.github.com>
2023-08-27 01:34:36 +0300

    add ThemeProvider to app.js, about component add the each article hover:scale-105, transform To CTA, and all header. add image to header. HeaderSocials  justify-center, create ListSkill component, change CarouselTransition error by changes the autoplayDelay from a string to a number

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cvFile from '../../assets/NickGofman_Resume_.pdf'; // Import the path to your CV file
 
 function CTA() {
+   const [socialsVisible, setSocialsVisible] = useState(false); // State to manage socials visibility
+
+   // Use useEffect to set socialsVisible to true after a delay
+   useEffect(() => {
+
+     setTimeout(() => {
+       setSocialsVisible(true);
+     }, 500); // Adjust the delay as needed
+   }, []);
   return (
-    <div className="flex items-center justify-center  mt-8 md:flex-row flex-col md:space-y-0 space-y-2  ">
-      <a
-        href={cvFile}
-        download="NickGofman_Resume.pdf"
-        className="btn  m-0  text-color-primary hover:bg-color-white hover:text-color-bg  md:mr-4 transition-colors duration-300 ease-in-out p-4 rounded-md border-2"
-      >
-        Download CV
-      </a>
-      <a
-        href="#contact"
-        className="btn  m-0 bg-color-primary hover:bg-color-white hover:text-color-bg text-color-bg hover:--color-white transition-colors duration-300 ease-in-out p-4 rounded-md"
-      >
-        Let's Talk
-      </a>
+    <div
+      className={`transition-transform ${
+        socialsVisible ? 'translate-x--100 ' : 'translate-x-full opacity-0'
+      } duration-1000 transform`}
+    >
+      <div className="  flex items-center justify-center  mt-8 md:flex-row flex-col md:space-y-0 space-y-2  ">
+        <a
+          href={cvFile}
+          download="NickGofman_Resume.pdf"
+          className="btn  m-0  text-color-primary hover:bg-color-white hover:text-color-bg  md:mr-4 transition-colors duration-300 ease-in-out p-4 rounded-md border-2"
+        >
+          Download CV
+        </a>
+        <a
+          href="#contact"
+          className="btn  m-0 bg-color-primary hover:bg-color-white hover:text-color-bg text-color-bg hover:--color-white transition-colors duration-300 ease-in-out p-4 rounded-md"
+        >
+          Let's Talk
+        </a>
+      </div>
     </div>
   );
 }
